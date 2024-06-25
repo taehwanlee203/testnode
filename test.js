b2500a716a334f04ae75fedb73e6e84a4c898c5a
taehwanlee203 <teein5579@naver.com>
2024-06-25 20:15:42 +0900

    수정

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
-const http = require('http');
+function multi_3(n) {
+  // 본인이 작성하라
+  for(i=1; i<=n; i++) {
+    if ( i%3 == 0)
+      document.write(i+" ");
+  }
+}
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+var x = Math.random()*10 + 10;
+var n = Math.floor(x);
+document.write("1에서 "+n+ "까지 숫자중에서 <br>")
+document.write("<hr>")
+document.write("3의 배수는 ")
+multi_3(n);
